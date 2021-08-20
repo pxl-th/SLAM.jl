1748da58f2ccb9ae9c2d1036f6851d1817518ce6
Anton Smirnov <anton.smn@protonmail.com>
2021-08-20 18:44:47 +0300

    Add visualizer struct

--- src/kitty.jl
+++ src/kitty.jl
@@ -54,12 +54,13 @@ end
 function get_camera_poses(dataset::KittyDataset)
     n_poses = length(dataset.poses)
     base_dir = SVector{3, Float64}(0, 0, 1)
+    base_point = SVector{4, Float64}(0, 0, 0, 1)
 
     positions = Vector{SVector{3, Float64}}(undef, n_poses)
     directions = Vector{SVector{3, Float64}}(undef, n_poses)
     for (i, pose) in enumerate(dataset.poses)
-        @inbounds positions[i] = -(pose[1:3, 1:3]' * pose[1:3, 4])
-        @inbounds directions[i] = normalize(pose[1:3, 1:3]' * base_dir)
+        @inbounds positions[i] = (pose * base_point)[1:3]
+        @inbounds directions[i] = normalize(pose[1:3, 1:3] * base_dir)
     end
     positions, directions
 end
@@ -68,25 +69,3 @@ Base.length(dataset::KittyDataset) = length(dataset.poses)
 function Base.getindex(dataset::KittyDataset, i)
     joinpath(dataset.frames_dir, @sprintf("%.06d.png", i - 1)) |> load
 end
-
-function main()
-    base_dir = "/home/pxl-th/Downloads/kitty-dataset/"
-    sequence = "00"
-    dataset = KittyDataset(base_dir, sequence)
-
-    positions, directions = dataset |> get_camera_poses
-    positions = to_makie(positions)
-    directions = to_makie(directions)
-
-    set_theme!(theme_black())
-    figure = Figure(resolution=(512, 512))
-    axis = Axis3(figure[1, 1]; aspect=:data, elevation=1)
-    im_axis = Makie.Axis(figure[1, 2]; aspect=DataAspect())
-
-    lines!(axis, positions; color=1:length(positions), colormap=:autumn1)
-    image!(im_axis, rotr90(dataset[1]))
-
-    figure
-end
-
-main()
--- src/viz.jl
+++ src/viz.jl
@@ -1,44 +1,47 @@
 using GLMakie
 using Images
 
-# NOTE: z is up
+include("kitty.jl")
 
-function main()
-    set_theme!(theme_black())
+
+mutable struct Visualizer
+    figure::Figure
+
+    top_grid::GridLayout
+    bottom_grid::GridLayout
+
+    pc_axis::Axis3
+    image_axis::Makie.Axis
+
+    point_cloud::Observable{Vector{Point3f0}}
+    camera_positions::Observable{Vector{Point3f0}}
+    camera_directions::Observable{Vector{Point3f0}}
+end
+
+function Visualizer()
+    set_theme!(theme_minimal())
 
     figure = Figure(resolution=(512, 512))
     top_grid = figure[1, 1:2] = GridLayout()
     bottom_grid = figure[2, 1:2] = GridLayout()
 
-    points_axis = Axis3(
-        top_grid[1, 1]; aspect=:data,
-        azimuth=π/8, elevation=1,
-    )
+    # Top grid configuration.
+
+    pc_axis = Axis3(top_grid[1, 1]; aspect=:data, azimuth=π / 8, elevation=1)
     image_axis = Makie.Axis(
         top_grid[1, 2]; aspect=DataAspect(),
         leftspinevisible=false, rightspinevisible=false,
         bottomspinevisible=false, topspinevisible=false,
     )
 
-    points = Node(Point3f0[])
-    points_obj = meshscatter!(
-        points_axis, points; markersize=0.05, color=:blue,
-        marker=Rect3D(Vec3f0(0, 0, 0), Vec3f0(1, 1, 1)),
-    )
+    top_grid[1, 1, Top()] = Label(figure, "3D Map")
+    top_grid[1, 2, Top()] = Label(figure, "Tracked Keypoints")
 
-    camera_positions = Node(Point3f0[])
-    camera_directions = Node(Point3f0[])
-    arrows!(points_axis, camera_positions, camera_directions; color=:red)
-
-    image = Node(zeros(RGB{Float64}, 100, 100))
-    image!(image_axis, rotr90(image[]))
+    # Bottom grid configuration.
 
     camera_dir_element = MarkerElement(;color=:red, marker="→")
     keypoint_element = MarkerElement(;color=:green, marker="⬤")
     mappoint_element = MarkerElement(;color=:blue, marker="⬤")
-
-    top_grid[1, 1, Top()] = Label(figure, "3D Map")
-    top_grid[1, 2, Top()] = Label(figure, "Tracked Keypoints")
 
     Legend(
         bottom_grid[1, 1],
@@ -53,38 +56,94 @@ function main()
         orientation=:horizontal, tellheight=true,
     )
 
-    colsize!(top_grid, 2, Relative(1/3))
-    colsize!(bottom_grid, 1, Relative(1/2))
-    colsize!(bottom_grid, 2, Relative(1/2))
+    point_cloud = Node(Point3f0[])
+    camera_positions = Node(Point3f0[])
+    camera_directions = Node(Point3f0[])
+
+    points_obj = meshscatter!(
+        pc_axis, point_cloud; markersize=0.05, color=:blue,
+        marker=Rect3D(Vec3f0(0, 0, 0), Vec3f0(1, 1, 1)),
+    )
+    # arrows!(pc_axis, camera_positions, camera_directions; color=:red, quality=4)
+    lines!(pc_axis, camera_positions; color=:red, quality=1)
 
     trim!(figure.layout)
     hidedecorations!(image_axis)
 
-    display(figure)
+    colsize!(top_grid, 2, Relative(1/3))
+    colsize!(bottom_grid, 1, Relative(1/2))
+    colsize!(bottom_grid, 2, Relative(1/2))
+
+    Visualizer(
+        figure, top_grid, bottom_grid, pc_axis, image_axis,
+        point_cloud, camera_positions, camera_directions,
+    )
+end
+
+Base.display(v::Visualizer) = Base.display(v.figure)
+
+expand_point_cloud!(v::Visualizer, new_points::Vector{Point3f0}) =
+    v.point_cloud[] = append!(v.point_cloud[], new_points)
+
+update_image!(v::Visualizer, new_image) = image!(v.image_axis, new_image)
+
+function add_camera_position!(v::Visualizer, position, direction)
+    v.camera_positions[] = push!(v.camera_positions[], position)
+    v.camera_directions[] = push!(v.camera_directions[], direction)
+end
+
+update_axii!(v::Visualizer) = v.pc_axis |> Makie.autolimits!
+
+# NOTE: z is up
+
+function main()
+    visualizer = Visualizer()
+    visualizer |> display
     sleep(1)
 
     reader = VideoIO.openvideo("/home/pxl-th/projects/slam.mp4")
 
     for (i, frame) in enumerate(reader)
-        image!(image_axis, rotr90(imresize(frame; ratio=0.3)))
-
-        camera_positions[] = push!(camera_positions[],
+        update_image!(visualizer, rotr90(imresize(frame; ratio=0.3)))
+        add_camera_position!(
+            visualizer,
             Point3f0(4 + rand() * 0.1, 0.5 + i, 2 + rand() * 0.01),
-        )
-        camera_directions[] = push!(camera_directions[],
             Point3f0(rand() * 0.2, 0.4 + rand() * 0.2, rand() * 0.2),
         )
-        new_points = [Point3f0(
+        expand_point_cloud!(visualizer, [Point3f0(
             rand() * 6 + 1, rand() * 2 + i, rand() * 3 + 0.5
-        ) for _ in 1:10]
-        points[] = append!(points[], new_points)
-        points_axis |> autolimits!
+        ) for _ in 1:10])
 
+        visualizer |> update_axii!
         sleep(1 / 5)
     end
 
     reader |> close
-
-    nothing
 end
-main()
+
+function kitty_vis()
+    visualizer = Visualizer()
+    visualizer |> display
+    sleep(1)
+
+    base_dir = "/home/pxl-th/Downloads/kitty-dataset/"
+    sequence = "02"
+    dataset = KittyDataset(base_dir, sequence)
+
+    positions, directions = dataset |> get_camera_poses
+    positions = to_makie(positions)
+    directions = to_makie(directions)
+
+    for i in 1:length(dataset)
+        frame = dataset[i]
+        update_image!(visualizer, rotr90(imresize(frame; ratio=0.3)))
+        visualizer.camera_positions[] = push!(
+            visualizer.camera_positions[], positions[i],
+        )
+
+        visualizer |> update_axii!
+        sleep(1 / 60)
+    end
+end
+kitty_vis()
+# main()
